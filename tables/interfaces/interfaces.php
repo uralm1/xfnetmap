05a3c45090a1ce00ca0566c53c9fa728dd97972f
uralm1 <uralm1@gmail.com>
2020-05-25 21:01:20 +0500

    Removed routes from interfaces

--- tables/interfaces/interfaces.php
+++ tables/interfaces/interfaces.php
@@ -5,41 +5,5 @@ class tables_interfaces {
   }
 
   //----------------------------------------------
-  function routes__parse($value) {
-    if (is_array($value)) {
-      return $value;
-    } else {
-      $arr2 = array();
-      if (!empty($value)) {
-        $arr1 = explode(';', $value);
-        foreach ($arr1 as $v) {
-  	  list($target, $netmask, $gateway) = explode(',', $v);
-          array_push($arr2, ['target' => $target, 'netmask' => $netmask, 'gateway' => $gateway]);
-        }
-      }
-      return $arr2;
-    }
-  }
-
-  function routes__serialize($value) {
-    if (is_array($value)) {
-      $arr1 = array();
-      foreach ($value as $v) {
-        array_push($arr1, trim($v['target']).','.trim($v['netmask']).','.trim($v['gateway']));
-      }
-      return implode(';', $arr1);
-    }
-    else
-      return '';
-  }
-
-  function routes__toString($value) {
-    $arr1 = array();
-    foreach ($value as $v) {
-      array_push($arr1, $v['target'].' / '.$v['netmask'].' via '.$v['gateway']);
-    }
-    return implode("\n", $arr1);
-  }
-  //----------------------------------------------
 }
 ?>
